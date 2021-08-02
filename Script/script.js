f899cb9c6d0491c47f2d7da670d0034afae74965
Hiu Lam <hiumathy.lam@flosports.tv>
2021-08-02 16:14:30 -0500

    modulate website

--- Script/script.js
+++ Script/script.js
@@ -1,73 +1,18 @@
 $(document).ready(function(){
-	$('.hamburger-icon').on('click', function(){
-		$(this).toggleClass('open');
-		$('.mobile-nav').toggleClass('open');
-		$('.mobile-nav li').toggleClass('open');
-	});
-
-	// Nav bar
-	$('.nav-home').on('click', function() {
-		$('html, body').animate({
-			scrollTop: $('.main').offset().top
-		}, 1000);
-	});
-
-	$('.nav-about').on('click', function(){
-		$('html, body').animate({
-			scrollTop: $(".supporting").offset().top
-		}, 1000);
-	});
-
-	$('.nav-port').on('click', function(){
-		$('html, body').animate({
-			scrollTop: $('.portfolio').offset().top
-		}, 1000);
-	});
-
-	$('.nav-contact').on('click', function(){
-		$('html, body').animate({
-			scrollTop: $('.logo_box').offset().top
-		}, 1000);
-	});
-
+	$('.hamburger-icon').on('click', toggleMenu );
+	
 	// mobile nav
-	$('.mnav-home').on('click', function(){
-		$('html, body').animate({
-			scrollTop: $('.main').offset().top
-		}, 1000);
+	$('.mnav-home').on('click', toggleMenu );
 
+	$('.mnav-about').on('click', toggleMenu );
+
+	$('.mnav-port').on('click', toggleMenu );
+
+	$('.mnav-contact').on('click', toggleMenu );
+
+	function toggleMenu() {
 		$('.hamburger-icon').toggleClass('open');
 		$('.mobile-nav').toggleClass('open');
 		$('.mobile-nav li').toggleClass('open');
-	});
-
-	$('.mnav-about').on('click', function(){
-		$('html, body').animate({
-			scrollTop: $('.supporting').offset().top
-		}, 1000);
-
-		$('.hamburger-icon').toggleClass('open');
-		$('.mobile-nav').toggleClass('open');
-		$('.mobile-nav li').toggleClass('open');
-	});
-
-	$('.mnav-port').on('click', function(){
-		$('html, body').animate({
-			scrollTop: $('.portfolio').offset().top
-		}, 1000);
-
-		$('.hamburger-icon').toggleClass('open');
-		$('.mobile-nav').toggleClass('open');
-		$('.mobile-nav li').toggleClass('open');
-	});
-
-	$('.mnav-contact').on('click', function(){
-		$('html, body').animate({
-			scrollTop: $('.logo_box').offset().top
-		}, 1000);
-
-		$('.hamburger-icon').toggleClass('open');
-		$('.mobile-nav').toggleClass('open');
-		$('.mobile-nav li').toggleClass('open');
-	});
+	}
 });
